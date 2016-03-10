7b6d7211ced83223689413c1188abc93612ce3f4
ben Lai <benr_18@hotmail.com>
2016-03-11 02:04:28 +0800

    add image
    add image viewer to see the camera capture

--- app/src/main/java/hk/ust/sight/starbugsv0/ImagesOverview.java
+++ app/src/main/java/hk/ust/sight/starbugsv0/ImagesOverview.java
@@ -1,12 +1,14 @@
 package hk.ust.sight.starbugsv0;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 
 public class ImagesOverview extends AppCompatActivity {
@@ -23,6 +25,17 @@ public class ImagesOverview extends AppCompatActivity {
                 dispatchTakePictureIntent();
                          }
                  });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            ImageView img= (ImageView) findViewById(R.id.mimage);
+           img.setImageBitmap(imageBitmap);
+        }
     }
 
 
