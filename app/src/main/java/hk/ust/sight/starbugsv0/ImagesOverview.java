bab0755e57617893bd8597483033900cc08cc758
ben Lai <benr_18@hotmail.com>
2016-03-11 01:49:17 +0800

    camera added
    add camera ,add debugger, run on sony device

--- app/src/main/java/hk/ust/sight/starbugsv0/ImagesOverview.java
+++ app/src/main/java/hk/ust/sight/starbugsv0/ImagesOverview.java
@@ -1,7 +1,13 @@
 package hk.ust.sight.starbugsv0;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.View;
+
 
 public class ImagesOverview extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class ImagesOverview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images_overview);
+
+        findViewById(R.id.button8).setOnClickListener(new View.OnClickListener() {
+
+
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+                         }
+                 });
+    }
+
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
