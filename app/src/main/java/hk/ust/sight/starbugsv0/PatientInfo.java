a7dc849fe83007f090e102661af628f8098cb907
StarBugs <agathaflc>
2016-03-24 19:34:08 +0800

    added set date button

--- app/src/main/java/hk/ust/sight/starbugsv0/PatientInfo.java
+++ app/src/main/java/hk/ust/sight/starbugsv0/PatientInfo.java
@@ -1,11 +1,24 @@
 package hk.ust.sight.starbugsv0;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class PatientInfo extends AppCompatActivity {
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    //private TextView dateOfBirth;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,4 +51,36 @@ public class PatientInfo extends AppCompatActivity {
         });
     }
 
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2 + 1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        TextView birthDate = (TextView) findViewById(R.id.dateOfBirth);
+        birthDate.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
 }
