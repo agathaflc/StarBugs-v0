caf150e6e069715f17899317723829aac1eed215
StarBugs <agatha.flc@hotmail.com>
2016-03-31 20:47:09 +0800

    added image upload for left eye, but still have bugs when selecting picture from gallery

--- app/src/main/java/hk/ust/sight/starbugsv0/ImagesOverview.java
+++ app/src/main/java/hk/ust/sight/starbugsv0/ImagesOverview.java
@@ -12,6 +12,8 @@ import android.support.v4.content.CursorLoader;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -27,25 +29,28 @@ public class ImagesOverview extends AppCompatActivity {
         setContentView(R.layout.activity_images_overview);
     }
 
-    private void selectImage() {
-        final CharSequence[] items = { "Take Photo", "Choose from Gallery", "Cancel" };
+    public static final int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+
+    public void selectImage(View view) {
+        final CharSequence[] items = {"Take Photo", "Choose from Gallery", "Cancel"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(ImagesOverview.this);
         builder.setTitle("Add Photo!");
         builder.setItems(items, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int item) {
-                int REQUEST_CAMERA = 0;
+
                 if (items[item].equals("Take Photo")) {
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(intent, REQUEST_CAMERA);
                 } else if (items[item].equals("Choose from Gallery")) {
-                    Intent intent = new Intent(
+                    /*Intent intent = new Intent(
                             Intent.ACTION_PICK,
                             android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    intent.setType("image/*");
+                    intent.setType("image/*");*/
+                    Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                     startActivityForResult(
-                            Intent.createChooser(intent, "Select File"),
+                            Intent.createChooser(i, "Select File"),
                             SELECT_FILE);
                 } else if (items[item].equals("Cancel")) {
                     dialog.dismiss();
@@ -56,7 +61,7 @@ public class ImagesOverview extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             if (requestCode == REQUEST_CAMERA) {
@@ -79,18 +84,25 @@ public class ImagesOverview extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
+                ImageView ivImage = (ImageView) findViewById(R.id.leftDiscAtCenter);
                 ivImage.setImageBitmap(thumbnail);
 
             } else if (requestCode == SELECT_FILE) {
-                Uri selectedImageUri = data.getData();
-                String[] projection = { MediaStore.MediaColumns.DATA };
-                CursorLoader cursorLoader = new CursorLoader(this,selectedImageUri, projection, null, null,
+                /*Uri selectedImageUri = data.getData();
+                String[] projection = {MediaStore.MediaColumns.DATA};
+                CursorLoader cursorLoader = new CursorLoader(this, selectedImageUri, projection, null, null,
                         null);
-                Cursor cursor =cursorLoader.loadInBackground();
+                Cursor cursor = cursorLoader.loadInBackground();
                 int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-                cursor.moveToFirst();
+                cursor.moveToFirst();*/
 
-                String selectedImagePath = cursor.getString(column_index);
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+                Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+
+                String selectedImagePath = cursor.getString(columnIndex);
 
                 Bitmap bm;
                 BitmapFactory.Options options = new BitmapFactory.Options();
@@ -105,9 +117,11 @@ public class ImagesOverview extends AppCompatActivity {
                 options.inJustDecodeBounds = false;
                 bm = BitmapFactory.decodeFile(selectedImagePath, options);
 
+                ImageView ivImage = (ImageView) findViewById(R.id.leftDiscAtCenter);
                 ivImage.setImageBitmap(bm);
             }
         }
 
 
     }
+}
