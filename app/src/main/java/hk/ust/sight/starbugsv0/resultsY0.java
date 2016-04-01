8fec1a8642f9c622e45593a4e4086f3932e87ae3
charlottechung <cwychung@ust.hk>
2016-04-01 12:37:51 +0800

    added result pages

---
+++ app/src/main/java/hk/ust/sight/starbugsv0/resultsY0.java
@@ -0,0 +1,13 @@
+package hk.ust.sight.starbugsv0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class resultsY0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_y0);
+    }
+}
