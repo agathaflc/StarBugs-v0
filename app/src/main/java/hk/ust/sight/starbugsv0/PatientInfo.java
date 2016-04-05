d06813425fae9a0e6b5a34c745330626f666fdcc
StarBugs <agatha.flc@hotmail.com>
2016-04-06 03:45:26 +0800

    create JSON, but not saved anywhere yet

--- app/src/main/java/hk/ust/sight/starbugsv0/PatientInfo.java
+++ app/src/main/java/hk/ust/sight/starbugsv0/PatientInfo.java
@@ -9,8 +9,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Calendar;
 
@@ -18,26 +23,16 @@ public class PatientInfo extends AppCompatActivity {
 
     private DatePicker datePicker;
     private Calendar calendar;
+    private int year;
     private int month;
     private int day;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_info);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
 
-        // Commented this part out because it's causing error and idk how to fix
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
 
         findViewById(R.id.btnBack1).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,13 +65,45 @@ public class PatientInfo extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Please Enter Clinic Name", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    //// RETRIEVE THE REMAINING DATA ////
+
+                    // Retrieve gender: gender will be String "Male" or "Female" in patientGender
+                    String patientGender;
+                    int selectedGenderId = ((RadioGroup)findViewById(R.id.radioGender)).getCheckedRadioButtonId();
+                    RadioButton genderButton = (RadioButton) findViewById(selectedGenderId);
+                    patientGender = genderButton.getText().toString();
+
+                    // Date of Birth: all stored in variables year, month, day
+                    // retrieve phone number
+                    EditText phoneNum = (EditText) findViewById(R.id.editPhoneNumber);
+                    String phoneNumString = phoneNum.toString().trim();
+
+                    // Write into JSON file
+                    JSONObject patient = new JSONObject();
+                    try {
+                        patient.put("lastName", lastName.getText().toString().trim());
+                        patient.put("firstName", firstName.getText().toString().trim());
+                        patient.put("middleName", middleName.getText().toString().trim());
+                        patient.put("bpjsNumber", bpjsNumber.getText().toString().trim());
+                        patient.put("gender", patientGender);
+                        patient.put("birthYear", year);
+                        patient.put("birthMonth", month);
+                        patient.put("birthDay", day);
+                        patient.put("phoneNumber", phoneNumString);
+                        patient.put("clinicName", clinicName.getText().toString().trim());
+
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    // Go to the next activity
                     startActivity(new Intent(PatientInfo.this, StartTutorial.class));
                 }
             }
         });
     }
 
-
+    // Code to get date: http://www.tutorialspoint.com/android/android_datepicker_control.htm
     @SuppressWarnings("deprecation")
     public void setDate(View view) {
         showDialog(999);
@@ -101,6 +128,9 @@ public class PatientInfo extends AppCompatActivity {
             // arg1 = year
             // arg2 = month
             // arg3 = day
+            year = arg1;
+            month = arg2;
+            day = arg3;
             showDate(arg1, arg2 + 1, arg3);
         }
     };
