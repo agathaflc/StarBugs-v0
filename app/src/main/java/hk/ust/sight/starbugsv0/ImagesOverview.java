36c45d48f6cae185ad1bdeca9ae4fe7d10b839e1
ben Lai <benr_18@hotmail.com>
2016-04-07 18:03:42 +0800

    add one more page to display all the process

--- app/src/main/java/hk/ust/sight/starbugsv0/ImagesOverview.java
+++ app/src/main/java/hk/ust/sight/starbugsv0/ImagesOverview.java
@@ -33,6 +33,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Vector;
 
+import org.opencv.core.CvType;
 
 public class ImagesOverview extends AppCompatActivity {
 
@@ -41,97 +42,109 @@ public class ImagesOverview extends AppCompatActivity {
 
     int i = 0;
 
-    public void GradedImage(View view) {
-        InputStream is = this.getResources().openRawResource(R.drawable.a);
-        Bitmap y1 = BitmapFactory.decodeStream(is);
-
-        ImageView ivImage = (ImageView) findViewById(R.id.rightDiscAtCenter);
-
-        ivImage.setImageBitmap(y1);
-
-        if (!OpenCVLoader.initDebug()) {
-            Log.d("ERROR", "Unable to load OpenCV");
-        }
-        int scale = 1;
-        int delta = 0;
-        int ddepth = 3;
-
-        Mat src = new Mat();
-        Mat gray = new Mat();
-        Vector<Mat> channels = new Vector<Mat>(3);
-
-        Utils.bitmapToMat(y1, src);
-
-
-
-        Imgproc.GaussianBlur(src, src, new Size(3, 3), 0, 0, Core.BORDER_DEFAULT);
-
-        Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
-
-        Mat grad_x = new Mat();
-        Mat grad_y = new Mat();
-        Mat abs_grad_x = new Mat();
-        Mat abs_grad_y = new Mat();
+//    public void GradedImage(View view) {
+//        InputStream is = this.getResources().openRawResource(R.drawable.a);
+//        Bitmap y1 = BitmapFactory.decodeStream(is);
 //
-       Imgproc.Sobel(gray, grad_x, ddepth, 1, 0, 3, scale, delta, Core.BORDER_DEFAULT);
-        Core.convertScaleAbs(grad_x, abs_grad_x);
-
-
-        Imgproc.Sobel(gray, grad_y, ddepth, 0, 1, 3, scale, delta, Core.BORDER_DEFAULT);
-        Core.convertScaleAbs(grad_y, abs_grad_y);
+//        ImageView ivImage = (ImageView) findViewById(R.id.rightDiscAtCenter);
 //
-        Mat grad = new Mat();
-        Core.addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+//        ivImage.setImageBitmap(y1);
 //
-//
-//        Core.split(src,channels);
-
-
-
-       // Imgproc.Sobel(footMat, dst, ddepth, dx, dy);
-        Bitmap bm = Bitmap.createBitmap(src.cols(), src.rows(), Bitmap.Config.ARGB_8888);
-
-        Utils.matToBitmap(grad, bm);
-
-
-        ivImage.setImageBitmap(bm);
-
-
-         //Imgproc.Sobel(tmp, dst, ddepth, dx, dy);
-
-
-        //  Bitmap thumbnail = BitmapFactory.decodeFile(mCurrentPhotoPath[0]);
-
-        //  ImageView ivImage = (ImageView) findViewById(R.id.rightDiscAtCenter);
-        //  ivImage.setImageBitmap(thumbnail);
-
-        // Bitmap thumbnail2 = thumbnail.copy(Bitmap.Config.ARGB_8888, true);
-        // Mat tmp =null;
-        // Utils.bitmapToMat(thumbnail2, tmp);
-
-
-        //Mat dst = null;
-        // int ddepth = -1; // destination depth. -1 maintains existing depth from source
-        // int dx = 1;
-        // int dy = 1;
-        // Imgproc.Sobel(tmp, dst, ddepth, dx, dy);
-
-        //  Imgproc.cvtColor(tmp, tmp, Imgproc.COLOR_RGB2GRAY);
-        //Imgproc.cvtColor(tmp, tmp, Imgproc.COLOR_GRAY2RGB, 4);
-        //  Utils.matToBitmap(tmp, thumbnail2);
-
-//        Bitmap bmp  = null;
-//        Mat tmp = null;
-//        try {
-//            //Imgproc.cvtColor(seedsImage, tmp, Imgproc.COLOR_RGB2BGRA);
-//            Imgproc.cvtColor(dst, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
-//            bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
-//            Utils.matToBitmap(tmp, bmp);
+//        if (!OpenCVLoader.initDebug()) {
+//            Log.d("ERROR", "Unable to load OpenCV");
 //        }
-//        catch (CvException e){}
-        // ivImage.setImageBitmap(thumbnail);
-
-    }
+//        int scale = 1;
+//        int delta = 0;
+//        int ddepth = CvType.CV_16S;
+//
+//        Mat src = new Mat();
+//        Mat gray = new Mat();
+//        Vector<Mat> channels = new Vector<Mat>(3);
+//
+//        Core.split(src, channels);
+//
+//        Utils.bitmapToMat(y1, src);
+//
+//       Mat red = new Mat();
+//
+//        Mat green = new Mat();
+//
+//        Mat blue = new Mat();
+//
+//        Core.extractChannel(src,red,0);
+//
+//        Core.extractChannel(src,green,1);
+//
+//        Core.extractChannel(src,blue,2);
+//
+//        Imgproc.GaussianBlur(red, red, new Size(3, 3), 0, 0, Core.BORDER_DEFAULT);
+//
+//       // Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
+//
+////        Mat grad_x = new Mat();
+////        Mat grad_y = new Mat();
+////        Mat abs_grad_x = new Mat();
+////        Mat abs_grad_y = new Mat();
+//////
+////       Imgproc.Sobel(red, grad_x, ddepth, 1, 0, 3, 10, 0.018, Core.BORDER_DEFAULT);
+////        Core.convertScaleAbs(grad_x, abs_grad_x);
+////
+////
+////        Imgproc.Sobel(red, grad_y, ddepth, 0, 1, 3, 10, 0.018, Core.BORDER_DEFAULT);
+////        Core.convertScaleAbs(grad_y, abs_grad_y);
+//////
+////        Mat grad = new Mat();
+////        Core.addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+////
+////
+////        Core.split(src,channels);
+//
+//
+//
+//       // Imgproc.Sobel(footMat, dst, ddepth, dx, dy);
+//        Bitmap bm = Bitmap.createBitmap(src.cols(), src.rows(), Bitmap.Config.ARGB_8888);
+//
+//        Utils.matToBitmap(red, bm);
+//
+//
+//        ivImage.setImageBitmap(bm);
+//
+//
+//         //Imgproc.Sobel(tmp, dst, ddepth, dx, dy);
+//
+//
+//        //  Bitmap thumbnail = BitmapFactory.decodeFile(mCurrentPhotoPath[0]);
+//
+//        //  ImageView ivImage = (ImageView) findViewById(R.id.rightDiscAtCenter);
+//        //  ivImage.setImageBitmap(thumbnail);
+//
+//        // Bitmap thumbnail2 = thumbnail.copy(Bitmap.Config.ARGB_8888, true);
+//        // Mat tmp =null;
+//        // Utils.bitmapToMat(thumbnail2, tmp);
+//
+//
+//        //Mat dst = null;
+//        // int ddepth = -1; // destination depth. -1 maintains existing depth from source
+//        // int dx = 1;
+//        // int dy = 1;
+//        // Imgproc.Sobel(tmp, dst, ddepth, dx, dy);
+//
+//        //  Imgproc.cvtColor(tmp, tmp, Imgproc.COLOR_RGB2GRAY);
+//        //Imgproc.cvtColor(tmp, tmp, Imgproc.COLOR_GRAY2RGB, 4);
+//        //  Utils.matToBitmap(tmp, thumbnail2);
+//
+////        Bitmap bmp  = null;
+////        Mat tmp = null;
+////        try {
+////            //Imgproc.cvtColor(seedsImage, tmp, Imgproc.COLOR_RGB2BGRA);
+////            Imgproc.cvtColor(dst, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
+////            bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
+////            Utils.matToBitmap(tmp, bmp);
+////        }
+////        catch (CvException e){}
+//        // ivImage.setImageBitmap(thumbnail);
+//
+//    }
 
     /**
      * Created by Ilya Gazman on 3/6/2016.
@@ -206,6 +219,15 @@ public class ImagesOverview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_images_overview);
+
+        findViewById(R.id.getGrade).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ImagesOverview.this, imageprocessing.class));
+            }
+        });
+
+
     }
 
     // The following code handles select image function.
