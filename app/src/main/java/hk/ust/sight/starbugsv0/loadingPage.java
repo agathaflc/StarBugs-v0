23a4ee17719ebfc73d5cf4f6935c0045ca83bc44
StarBugs <agatha.flc@hotmail.com>
2016-04-26 17:13:28 +0800

    fixed minor bugs

---
+++ app/src/main/java/hk/ust/sight/starbugsv0/loadingPage.java
@@ -0,0 +1,30 @@
+package hk.ust.sight.starbugsv0;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Handler;
+import java.util.logging.LogRecord;
+
+/**
+ * Created by StarBugs on 26/4/16.
+ */
+public class loadingPage extends AppCompatActivity {
+    private static int TIME_OUT = 2500; //Time to launch the another activity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_page);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(loadingPage.this, resultsY0.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
